b3fc6c313e81ce9b3ab780c9acfadaf1a3fe2678
Ishneet Singh <ishneet100@gmail.com>
2023-08-26 16:13:23 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,87 @@
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const handleSubmit = () => {};
+    const [titleText, setTitleText] = useState("");
+    const [taxText, setTaxText] = useState("");
+    const [contractText, setContractText] = useState("");
+    const [mortgageText, setMortgageText] = useState("");
 
-  return (
-    <>
-      <h1>Sale File</h1>
-      <form onSubmit={handleSubmit}>
-      <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+    const handleFileChange = async (e, setText) => {
+        const formData = new FormData();
+        formData.append("pdfFile", e.target.files[0]);
 
-      </form>
-    </>
-  );
+        try {
+            const response = await axios.post("http://localhost:3000/extract-text", formData);
+
+            if (response.status === 200) {
+                setText(response.data.text);
+            } else {
+                console.error("Failed to extract text from PDF.");
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    };
+
+    return (
+        <>
+            <h1>Sale File</h1>
+            <form>
+                <div>
+                    <label>Title Document:</label>
+                    <input
+                        type="file"
+                        className="file-input file-input-bordered w-full max-w-xs"
+                        onChange={(e) => handleFileChange(e, setTitleText)}
+                    />
+                </div>
+                <div>
+                    <label>Tax Document:</label>
+                    <input
+                        type="file"
+                        className="file-input file-input-bordered w-full max-w-xs"
+                        onChange={(e) => handleFileChange(e, setTaxText)}
+                    />
+                </div>
+                <div>
+                    <label>Contract Document:</label>
+                    <input
+                        type="file"
+                        className="file-input file-input-bordered w-full max-w-xs"
+                        onChange={(e) => handleFileChange(e, setContractText)}
+                    />
+                </div>
+                <div>
+                    <label>Mortgage Document:</label>
+                    <input
+                        type="file"
+                        className="file-input file-input-bordered w-full max-w-xs"
+                        onChange={(e) => handleFileChange(e, setMortgageText)}
+                    />
+                </div>
+            </form>
+            <div>
+                <h2>Extracted Text:</h2>
+                <div>
+                    <h3>Title Document:</h3>
+                    <p>{titleText}</p>
+                </div>
+                <div>
+                    <h3>Tax Document:</h3>
+                    <p>{taxText}</p>
+                </div>
+                <div>
+                    <h3>Contract Document:</h3>
+                    <p>{contractText}</p>
+                </div>
+                <div>
+                    <h3>Mortgage Document:</h3>
+                    <p>{mortgageText}</p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
