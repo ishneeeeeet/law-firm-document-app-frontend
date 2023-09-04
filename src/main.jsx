dff43048414229f2737757d5ae21aed7935ef834
ishneeeeeet <ishneet100@gmail.com>
2023-09-04 13:37:26 -0600

    modified:   package-lock.json 	modified:   package.json 	new file:   src/DocumentPage.jsx 	modified:   src/InputPage.jsx 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InputPage from "./InputPage.jsx";
+import DocumentPage from "./DocumentPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/input-page" element={<InputPage />} />
+        <Route path="/document" element={<DocumentPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
