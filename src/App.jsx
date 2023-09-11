8b1541c248bde78fb479b0372c32627a21b1f2e8
Ishneet Singh <ishneet100@gmail.com>
2023-09-10 19:13:42 -0600

    modified:   src/App.jsx 	modified:   src/pages/InputPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
-import InputPage from "./pages/InputPage";
 import SelectFile from "./pages/SelectFile";
 
 function App() {
-    // const [titleText, setTitleText] = useState("");
-    // const [taxText, setTaxText] = useState("");
-    // const [contractText, setContractText] = useState("");
-    // const [mortgageText, setMortgageText] = useState("");
-
-    // const handleFileChange = async (e, setText) => {
-    //     const formData = new FormData();
-    //     formData.append("pdfFile", e.target.files[0]);
-
-    //     try {
-    //         const response = await axios.post("https://law-firm-backend-ju32.onrender.com/extract-text", formData);
-
-    //         if (response.status === 200) {
-    //             setText(response.data.text);
-    //         } else {
-    //             console.error("Failed to extract text from PDF.");
-    //         }
-    //     } catch (error) {
-    //         console.error("An error occurred:", error);
-    //     }
-    // };
-
-    return (
-        <>
-            {/* <h1 className="text-red-700">Sale File</h1>
-            <form>
-                <div>
-                    <label>Title Document:</label>
-                    <input
-                        type="file"
-                        className="file-input file-input-bordered w-full max-w-xs"
-                        onChange={(e) => handleFileChange(e, setTitleText)}
-                    />
-                </div>
-                <div>
-                    <label>Tax Document:</label>
-                    <input
-                        type="file"
-                        className="file-input file-input-bordered w-full max-w-xs"
-                        onChange={(e) => handleFileChange(e, setTaxText)}
-                    />
-                </div>
-                <div>
-                    <label>Contract Document:</label>
-                    <input
-                        type="file"
-                        className="file-input file-input-bordered w-full max-w-xs"
-                        onChange={(e) => handleFileChange(e, setContractText)}
-                    />
-                </div>
-                <div>
-                    <label>Mortgage Document:</label>
-                    <input
-                        type="file"
-                        className="file-input file-input-bordered w-full max-w-xs"
-                        onChange={(e) => handleFileChange(e, setMortgageText)}
-                    />
-                </div>
-            </form>
-            <div>
-                <h2>Extracted Text:</h2>
-                <div>
-                    <h3>Title Document:</h3>
-                    <p>{titleText}</p>
-                </div>
-                <div>
-                    <h3>Tax Document:</h3>
-                    <p>{taxText}</p>
-                </div>
-                <div>
-                    <h3>Contract Document:</h3>
-                    <p>{contractText}</p>
-                </div>
-                <div>
-                    <h3>Mortgage Document:</h3>
-                    <p>{mortgageText}</p>
-                </div>
-            </div> */}
-            <SelectFile/>
-          
-        </>
-    );
+  return (
+    <>
+      <SelectFile />
+    </>
+  );
 }
 
 export default App;
--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,11 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios"; // Import Axios
 
 const InputPage = () => {
   const navigate = useNavigate();
 
-  const handleNavigation = () => {
-    navigate("/document");
+  const handleNavigation = async () => {
+    // Create FormData object to send files
+    const formData = new FormData();
+    formData.append("title", document.querySelector('input[type="file"][name="title"]').files[0]);
+    formData.append("tax", document.querySelector('input[type="file"][name="tax"]').files[0]);
+    formData.append("contract", document.querySelector('input[type="file"][name="contract"]').files[0]);
+    formData.append("mortgage", document.querySelector('input[type="file"][name="mortgage"]').files[0]);
+
+    try {
+      // Send POST request to the API
+      await axios.post("https://5sx3zskz1e.execute-api.us-east-1.amazonaws.com/dev/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data; boundary=<calculated when request is sent>", // Set the content type to multipart/form-data
+        },
+      });
+
+      // After successful upload, navigate to "/document"
+      navigate("/document");
+    } catch (error) {
+      console.error("Error uploading files:", error);
+    }
   };
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -21,142 +42,13 @@ const InputPage = () => {
           </p>
         </div>
         <div className="flex flex-wrap">
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <div className="flex items-center justify-center w-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <div className="flex items-center justify-center w-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <div className="flex items-center justify-center w-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <div className="flex items-center justify-center w-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </div>
+        
+  <input type="file" name="title" />
+  <input type="file" name="tax" />
+  <input type="file" name="contract" />
+  <input type="file" name="mortgage" />
+
+
         </div>
         <button
           onClick={handleNavigation}
