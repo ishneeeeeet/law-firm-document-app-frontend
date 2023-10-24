742afe568b6a888ce1d76b9a5f068110ee44bd30
Abhishek Jadhav <abhijad200@gmail.com>
2023-10-24 21:56:04 +0530

    Updated eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 6, sourceType: 'module', ecmaFeatures: {"modules": true} },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  "rules": {
+    "no-undef": "off",
+    "react/react-in-jsx-scope": "off"
   },
+  "extends": [
+    "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true,
+      "experimentalObjectRestSpread": true
+    }
+  }
 }
